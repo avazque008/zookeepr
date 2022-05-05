4f345ac2c1799d48ee514980a8c380a9cf1c00c9
Alex Vazquez <avazque008@outlook.com>
2022-05-05 17:09:42 -0400

    implemented front-end

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const fs = require('fs');
 const path = require('path');
 
+console.log(__dirname);
+
 const express = require('express');
 const app = express();
 
@@ -8,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+
+app.use(express.static('public'));
 
 const {animals} = require('./data/animals.json');
 
@@ -128,6 +132,23 @@ app.post('/api/animals', (req, res) => {
         res.json(animal);
     }
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
 
 
 app.listen(PORT, () => {
